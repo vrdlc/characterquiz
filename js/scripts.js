6f3aec92d3fca3b34946e3cf585cce4736e8328c
Jay Whang and Ramon de la Cruz <jaywhang83@gmail.com>
2016-01-14 16:28:59 -0800

    add different if else statements/code

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
     var nameSpace =["question1", "question2", "question3", "question4", "question5"];
     // var characters = [["Tony Stark", "Loki"], ["hulk", "Thor"], [""]];
     debugger;
-/*
+
     var question1 = $("input#question1").val();
     var question2 = $("input#question2").val();
     var question3 = $("input#question3").val();
     var question4 = $("input#question4").val();
     var question5 = $("input#question5").val();
-*/
 
+/*
     var counter = 0;
     nameSpace.forEach(function(index){
       var question = $("input#" + index).val();
@@ -19,6 +19,7 @@ $(document).ready(function() {
          counter++;
        }
     });
+
 
 
       if (counter === 0 ) {
@@ -45,6 +46,28 @@ $(document).ready(function() {
       else if (counter === 5 ) {
         $(".character").text("Deadpool");
         $("#answer").show();
+      }
+
+*/
+    if (question1 === "yes") {
+        $("#q3").show();
+        $("#q0").hide();
+      }
+    if(question1 === "no"){
+        $("#q2").show();
+      }
+
+      if (question2 === "yes") {
+        $("#q1").show();
+      }
+      if (question2 === "no") {
+        $("#q4").show();
+      }
+      if (question3 === "yes") {
+        $("#q1").show();
+      }
+      if (question3 === "no") {
+        $("#q2").show();
       }
 
         event.preventDefault();
